c0a5a14144942848e84a0b5e80b1017ef59da095
pabloVinicius <vinicius.pablo.18@gmail.com>
2017-12-06 11:11:34 -0300

    teste de pesquisa de conhecimento implementado

--- app/src/test/java/ufpi/br/swap/PrimeiraIteracaoUnitTest.java
+++ app/src/test/java/ufpi/br/swap/PrimeiraIteracaoUnitTest.java
@@ -1,13 +1,21 @@
 package ufpi.br.swap;
 
+import android.support.annotation.NonNull;
 import android.util.Log;
 
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 
 import retrofit2.Call;
 import retrofit2.Response;
 import ufpi.br.swap.controle.CadastroActivity;
 import ufpi.br.swap.controle.LoginActivity;
+import ufpi.br.swap.entidades.Conhecimento;
 import ufpi.br.swap.entidades.Usuario;
 import ufpi.br.swap.servico.MensagemAPI;
 import ufpi.br.swap.servico.RetrofitService;
@@ -90,5 +98,31 @@ public class PrimeiraIteracaoUnitTest {
         Call<Usuario> call4 = service4.login("new_email@mail.com", "senha");
         Response<Usuario> response4 = call4.execute();
         assertFalse(response4.body().getLogado());
+    }
+
+    @Test
+    public void pesquisaConhecimentoTeste() throws Exception{
+        RetrofitService service = ServiceGenerator.createService(RetrofitService.class);
+        Call<List<Conhecimento>> call = service.pesquisarConhecimentos("h");
+        Response<List<Conhecimento>> response = call.execute();
+
+        Conhecimento c1 = new Conhecimento();
+        c1.setName("Hacker");
+        c1.setUser("Marcos Joshoa");
+        c1.setDescription("Conhecimentos avançados em Hasckeragem");
+        c1.setRating(5.0);
+
+        Conhecimento c2 = new Conhecimento();
+        c2.setName("HTML5");
+        c2.setUser("Kássio Venícios");
+        c2.setDescription("Conhecimentos avançados em HTML5");
+        c2.setRating(5.0);
+
+
+
+        assertEquals(2,response.body().size());
+        assertEquals(c1.getName(),response.body().get(0).getName());
+        assertEquals(c2.getName(), response.body().get(1).getName());
+
     }
 }
