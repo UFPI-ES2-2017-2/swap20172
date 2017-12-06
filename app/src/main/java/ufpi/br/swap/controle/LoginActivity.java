8b116e9adf31d4ef33054036df969a11816a2936
KassioVenicios <kassio_venicios@hotmail.com>
2017-12-06 13:55:14 -0300

    Página login atualizada

--- app/src/main/java/ufpi/br/swap/controle/LoginActivity.java
+++ app/src/main/java/ufpi/br/swap/controle/LoginActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -102,7 +101,7 @@ public class LoginActivity extends AppCompatActivity {
             public void onResponse(Call<Usuario> call, Response<Usuario> response) {
                 if (response.isSuccessful()) {
                     Usuario usuario = response.body();
-                    if (usuario.getLogado()) {
+                    if (usuario.getLogged()) {
                         setStatus(true);
                         irParaTelaInicial(usuario);
                     } else {
@@ -125,7 +124,7 @@ public class LoginActivity extends AppCompatActivity {
     private void irParaTelaInicial(Usuario usuario) {
         Intent intent = new Intent(this, InicialActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        intent.putExtra("user_name", usuario.getNome());
+        intent.putExtra("user_name", usuario.getName());
         intent.putExtra("user_email", usuario.getEmail());
         startActivity(intent);
     }
